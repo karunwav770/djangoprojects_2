734d476944a16f13bd875990607a7bc6f1e0bb6b
karun <karun.wav@gmail.com>
2023-01-23 11:03:41 +0530

    views_updated

--- hlcproject/testapp/views.py
+++ hlcproject/testapp/views.py
@@ -8,3 +8,6 @@ def myfun(request):
     py_dict={'wish':'hello world'}
     json_resp=json.dumps(py_dict)
     return HttpResponse(json_resp)
+  
+def wish(request):
+    return HttpResponse("<h1>iam from wish</h1>")
